eb60aa8c6594b73be13abebea7bf8b456067b909
brianpeppard <>
2018-10-16 16:20:38 -0500

    adds material-ui

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,3 +1,5 @@
+import Button from '@material-ui/core/Button';
+
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
@@ -5,6 +7,9 @@ import Info from './Info.jsx';
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
     <Hello />
     <Info />
   </div>
